309911a4a415068ae52e814de140f784e15b0908
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2022-07-13 10:33:59 -0400

    initial commit

---
+++ src/pd.lean
@@ -0,0 +1,74 @@
+
+-- import algebra.ring
+import ring_theory.ideal.basic
+
+/-- Multiple choose -/
+def mchoose (m n : ℕ) : ℕ := finset.prod (finset.range m) (λ p, nat.choose (p * n + n - 1) (n - 1))
+
+lemma mchoose_zero (n : ℕ) : mchoose 0 n = 1 := 
+begin
+  rw mchoose, simp, 
+end
+
+lemma mchoose_succ (m n : ℕ) : mchoose (m + 1) n = (nat.choose (m * n + n - 1) (n - 1)) * (mchoose m n) := 
+begin
+  simp [mchoose],
+  rw finset.prod_range_succ, 
+  simp, 
+  rw mul_comm, 
+end
+
+lemma mchoose_lemma (m n : ℕ) (hn : 1 ≤ n): (m.factorial) * (n.factorial) ^m * (mchoose m n) = (m * n).factorial :=
+begin
+  induction m with m ih, 
+  simp only [nat.nat_zero_eq_zero, nat.factorial_zero, pow_zero, one_mul, zero_mul], apply mchoose_zero, 
+  simp only [nat.factorial_succ], rw [mchoose_succ, pow_succ],
+  simp only [←mul_assoc],
+  conv_rhs { rw nat.succ_mul},
+  rw ← nat.add_choose_mul_factorial_mul_factorial,
+  rw ← ih,
+  suffices : (m + 1) * (m * n + n - 1).choose (n - 1)
+    = (m * n + n).choose n,
+  rw ← this,
+  ring_nf,
+
+  suffices : 0 < (m * n).factorial * n.factorial,
+  rw ← nat.mul_left_inj this, 
+  simp only [←mul_assoc],
+  rw nat.add_choose_mul_factorial_mul_factorial,
+  rw ← nat.mul_factorial_pred hn,
+  rw mul_comm n _, 
+  rw ← mul_assoc,
+  suffices this : m * n + n - 1 =  m * n + (n - 1),
+  { rw this,
+    simp only [mul_assoc], rw mul_comm, 
+    simp  only [← mul_assoc],
+    rw nat.add_choose_mul_factorial_mul_factorial,
+    suffices also_this : 0 < m * n + n,
+    { rw ← nat.mul_factorial_pred  also_this, 
+      rw ← this, 
+      ring },
+    apply nat.add_pos_right _ hn, },
+  exact nat.add_sub_assoc hn (m * n),
+
+  apply nat.mul_pos;
+  apply nat.factorial_pos
+end
+
+/-- The divided power structure on an ideal I of a commutative ring A -/
+structure has_divided_powers (A : Type*) [comm_ring A] (I : ideal A) := 
+(div_pow : ℕ → I → A)
+(div_pow_zero : div_pow 0 = 1)
+(div_pow_one : div_pow 1 = coe)
+(div_pow_mem : ∀ (n : ℕ) (x : I), 1 ≤ n → div_pow n x ∈ I)
+(div_pow_sum : ∀ (n : ℕ) (x y : I), div_pow n (x + y)
+  = finset.sum (finset.range (n + 1)) (λ k, (div_pow k x) * (div_pow (n - k) y)))
+(div_pow_smul : ∀ (n : ℕ) (a : A) (x : I), div_pow n (a • x) = (a ^ n) * (div_pow n x))
+(div_pow_mul : ∀ (m n : ℕ) (x : I), (div_pow m x) * (div_pow n x) = (nat.choose (n+m) m) * div_pow (m + n) x)
+(div_pow_comp : ∀ (m n : ℕ) (hn : 1 ≤ n) (x : I),
+  div_pow m (⟨div_pow n x, div_pow_mem n x hn⟩) = (mchoose m n) * div_pow (m * n) x)
+
+
+#print has_divided_powers
+
+#lint
