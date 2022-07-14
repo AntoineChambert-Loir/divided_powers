0768b60ee622a850913fdceb74b3b2c8f25c9abf
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2022-07-14 16:09:08 -0400

    possible refactor

--- src/pd.lean
+++ src/pd.lean
@@ -40,8 +40,7 @@ begin
 end
 
 /-- The divided power structure on an ideal I of a commutative ring A -/
-structure has_divided_powers {A : Type*} [comm_ring A] (I : ideal A) := 
-(dpow : ℕ → I → A)
+structure divided_powers {A : Type*} [comm_ring A] {I : ideal A} (dpow : ℕ → I → A) : Prop := 
 (dpow_zero : dpow 0 = 1)
 (dpow_one : dpow 1 = coe)
 (dpow_mem : ∀ (n : ℕ) (x : I), 1 ≤ n → dpow n x ∈ I)
@@ -52,49 +51,49 @@ structure has_divided_powers {A : Type*} [comm_ring A] (I : ideal A) :=
 (dpow_comp : ∀ (m n : ℕ) (hn : 1 ≤ n) (x : I),
   dpow m (⟨dpow n x, dpow_mem n x hn⟩) = (mchoose m n) * dpow (m * n) x)
 
-namespace has_divided_powers
-variables {A : Type*} [comm_ring A] {I : ideal A} (hI : has_divided_powers I)
+namespace divided_powers
+variables {A : Type*} [comm_ring A] {I : ideal A} (dpow : ℕ → I → A) (hI : divided_powers dpow)
 include hI
 
-lemma factorial_mul_dpow_eq_pow (n : ℕ) (x : I) : (n.factorial : A) * (hI.dpow n x) = x^n :=
+lemma factorial_mul_dpow_eq_pow (n : ℕ) (x : I) : (n.factorial : A) * (dpow n x) = x^n :=
 begin
   induction n with n ih,
-  { rw [pow_zero, nat.factorial_zero, nat.cast_one, one_mul, dpow_zero, pi.one_apply] },
+  { rw [pow_zero, nat.factorial_zero, nat.cast_one, one_mul, hI.dpow_zero, pi.one_apply] },
   { rw [nat.factorial_succ, mul_comm (n + 1), nat.cast_mul, mul_assoc, pow_succ', ← ih, mul_assoc,
-      ← (n + 1).choose_one_right, nat.succ_eq_add_one, ← hI.dpow_mul, dpow_one, mul_comm (x : A)], }
+      ← (n + 1).choose_one_right, nat.succ_eq_add_one, ← hI.dpow_mul, hI.dpow_one,
+      mul_comm (x : A)], }
 end
 
-lemma dpow_eval_zero {n : ℕ} (hn : 0 < n) : hI.dpow n 0 = 0 := 
+lemma dpow_eval_zero {n : ℕ} (hn : 0 < n) : dpow n 0 = 0 := 
 by rw [← smul_zero (0 : A), hI.dpow_smul, zero_pow hn, zero_mul]
 
 structure is_pd_morphism {A B : Type*} [comm_ring A] [comm_ring B] (I : ideal A) (J : ideal B )
-  [hI : has_divided_powers I] [hJ : has_divided_powers J] (f : A →+* B) :=
+  (dpow_I : ℕ → I → A) (dpow_J : ℕ → J → B)
+  (hI : divided_powers dpow_I) (hJ : divided_powers dpow_J) (f : A →+* B) :=
 (ideal_comp : ∀ (a : I), f a ∈ J)
-(dpow_comp : ∀ (n : ℕ) (a : I), hJ.dpow n (⟨f a, ideal_comp a⟩) = f (hI.dpow n a))
+(dpow_comp : ∀ (n : ℕ) (a : I), dpow_J n (⟨f a, ideal_comp a⟩) = f (dpow_I n a))
 
 structure pd_morphism {A B : Type*} [comm_ring A] [comm_ring B] {I : ideal A} {J : ideal B }
-  (hI : has_divided_powers I) (hJ : has_divided_powers J) :=
+  (dpow_I : ℕ → I → A) (dpow_J : ℕ → J → B) (hI : divided_powers dpow_I)
+  (hJ : divided_powers dpow_J) :=
 (to_ring_hom : A →+* B)
 (ideal_comp : ∀ (a : I), to_ring_hom a ∈ J)
 (dpow_comp : ∀ (n : ℕ) (a : I), 
-  hJ.dpow n (⟨to_ring_hom a, ideal_comp a⟩) = to_ring_hom (hI.dpow n a))
+  dpow_J n (⟨to_ring_hom a, ideal_comp a⟩) = to_ring_hom (dpow_I n a))
 
 --notation `(` A `,` I, `,` hI `)` →ₚ  `(` B `,` J, `,` hJ `)` := pd_morphism hI hJ
 
 structure is_sub_pd_ideal (J : ideal A) : Prop :=
 (is_sub_ideal : ∀ j : J, (j : A) ∈ I)
-(dpow_mem_ideal : ∀ (n : ℕ) (hn : 0 < n) (j : J), hI.dpow n ⟨j, is_sub_ideal j⟩ ∈ J )
+(dpow_mem_ideal : ∀ (n : ℕ) (hn : 0 < n) (j : J), dpow n ⟨j, is_sub_ideal j⟩ ∈ J )
 
-
--- We solved the ideal quotient problem, but we need to refactor has_divided_powers so that it does
--- not contain any data
-/- 
-def quot.has_divided_powers (J : ideal A) (hIJ : is_sub_pd_ideal hI (I ⊓ J)) :
-  has_divided_powers (I.map (ideal.quotient.mk J)) ↔ false := sorry -/
+def quot.has_divided_powers (J : ideal A) (hIJ : is_sub_pd_ideal dpow hI (I ⊓ J)) :
+  ∃ (dpow_quot : ℕ → (I.map (ideal.quotient.mk J)) → (A ⧸ J)),
+  divided_powers dpow_quot ↔ false := sorry
 
 lemma is_sub_pd_ideal_iff (S : set A) (hS : S ⊆ I) :
-  is_sub_pd_ideal hI (ideal.span S) ↔ 
-  ∀ (n : ℕ) (hn : 0 < n) (s : S), hI.dpow n ⟨s, hS s.property⟩ ∈ ideal.span S :=
+  is_sub_pd_ideal dpow hI (ideal.span S) ↔ 
+  ∀ (n : ℕ) (hn : 0 < n) (s : S), dpow n ⟨s, hS s.property⟩ ∈ ideal.span S :=
 sorry
 
-end has_divided_powers
+end divided_powers
