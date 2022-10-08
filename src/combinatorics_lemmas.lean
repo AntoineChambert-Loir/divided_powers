17d4087d55f7864ca572b46bbf6a04124b1c31dc
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2022-10-08 17:20:20 +0200

    dpow on ideal_add is done !

--- src/combinatorics_lemmas.lean
+++ src/combinatorics_lemmas.lean
@@ -1,4 +1,5 @@
 import data.nat.choose.vandermonde
+import multinomial 
 
 section combinatorics
 
@@ -9,13 +10,17 @@ finset.prod (finset.range m) (λ p, nat.choose (p * n + n - 1) (n - 1))
 lemma mchoose_zero (n : ℕ) : mchoose 0 n = 1 := 
 by rw [mchoose, finset.range_zero, finset.prod_empty]
 
+lemma mchoose_zero' (m : ℕ) : mchoose m 0 = 1 :=
+by simp only [mchoose, mul_zero, nat.choose_self, finset.prod_const_one] 
+
 lemma mchoose_succ (m n : ℕ) : 
   mchoose (m + 1) n = (nat.choose (m * n + n - 1) (n - 1)) * (mchoose m n) := 
 by simp only [mchoose, finset.prod_range_succ, mul_comm]
 
-lemma mchoose_lemma (m : ℕ) {n : ℕ} (hn : 0 < n) :
+lemma mchoose_lemma (m : ℕ) {n : ℕ} (hn : n ≠ 0) :
   (m.factorial) * (n.factorial)^m * (mchoose m n) = (m * n).factorial :=
 begin
+  rw ← zero_lt_iff at hn,
   induction m with m ih,
   { rw [mchoose_zero, mul_one, zero_mul, nat.factorial_zero, pow_zero, mul_one] }, 
   { have hmn : (m + 1) * (m * n + n - 1).choose (n - 1) = (m * n + n).choose n,
@@ -29,7 +34,7 @@ begin
     rw [mchoose_succ, nat.factorial_succ, pow_succ, ← mul_assoc],
     conv_rhs { rw nat.succ_mul},
     rw [← nat.add_choose_mul_factorial_mul_factorial, ← ih, ← hmn],
-    ring_nf, }
+    ring_nf, } 
 end
 
 end combinatorics
