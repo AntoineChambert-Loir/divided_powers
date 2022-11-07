87d5b63ac6a57b9e6a3d2aebde242d4fbd3e6393
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2022-11-07 16:41:37 +0000

    change M × ℕ  to ℕ × M

--- src/divided_powers/dp_algebra.lean
+++ src/divided_powers/dp_algebra.lean
@@ -15,6 +15,8 @@ noncomputable theory
 /-! 
 The divided power algebra of a module -/
 
+open mv_polynomial
+
 
 variables (R : Type*) [comm_ring R]
 variables (M : Type*) [add_comm_monoid M] [module R M]
@@ -22,18 +24,14 @@ variables (M : Type*) [add_comm_monoid M] [module R M]
 namespace divided_power_algebra
 
 /- TODO : change to ℕ × M because (x,n) represents dpow n x -/
-inductive rel : mv_polynomial (M × ℕ) R → mv_polynomial (M × ℕ) R → Prop
+inductive rel : mv_polynomial (ℕ × M) R → mv_polynomial (ℕ × M) R → Prop
 -- force `ι` to be linear and creates the divided powers
-| zero {a : M} : 
-  rel (mv_polynomial.X (a, 0)) 1
-| smul {r : R} {n : ℕ} {a : M} :
-  rel (mv_polynomial.X (r • a, n)) (r^n • mv_polynomial.X (a, n))
-| mul {m n : ℕ} {a : M} : 
-  rel (mv_polynomial.X (a,m) * mv_polynomial.X (a, n))
-    ((nat.choose (m + n) n) • mv_polynomial.X (a, m + n))
-| add {n : ℕ} {a b : M} :
-  rel (mv_polynomial.X (a+b,n)) 
-    (finset.sum (finset.range (n + 1)) (λ k, (mv_polynomial.X (a, k) * mv_polynomial.X (b, n - k))))
+| zero {a : M} : rel (X (0, a)) 1
+| smul {r : R} {n : ℕ} {a : M} : rel (X (n, r • a)) (r^n • X (n, a))
+| mul {m n : ℕ} {a : M} : rel (X (m, a) * X (n, a))
+  ((nat.choose (m + n) n) • X (m + n, a))
+| add {n : ℕ} {a b : M} : rel (X (n, a+b)) 
+  (finset.sum (finset.range (n + 1)) (λ k, (X (k, a) * X (n - k, b))))
 
 end divided_power_algebra
 
@@ -48,7 +46,7 @@ variable {M}
 The canonical linear map `M →ₗ[R] divided_power_algebra R M`.
 -/
 def ι : M →ₗ[R] (divided_power_algebra R M) :=
-{ to_fun := λ m, (ring_quot.mk_alg_hom R _ (mv_polynomial.X (m, 1))),
+{ to_fun := λ m, (ring_quot.mk_alg_hom R _ (X (1, m))),
   map_add' := λ x y, by { 
     rw [←alg_hom.map_add, ring_quot.mk_alg_hom_rel R rel.add],
     simp only [finset.sum_range_succ', finset.sum_range_zero, zero_add, nat.sub_zero,
@@ -61,13 +59,13 @@ def ι : M →ₗ[R] (divided_power_algebra R M) :=
     simp only [ring_hom.id_apply, alg_hom.map_smul], } }
 
 lemma ring_quot_mk_alg_hom_mv_polynomial_ι_eq_ι (m : M) :
-  ring_quot.mk_alg_hom R (rel R M) (mv_polynomial.X (m, 1)) = ι R m := rfl
+  ring_quot.mk_alg_hom R (rel R M) (X (1, m)) = ι R m := rfl
 
 variable (M)
 def grade (n : ℕ) : submodule R (divided_power_algebra R M) :=
 submodule.span R 
-  { u : divided_power_algebra R M | ∃ (s : finset (M × ℕ)) (hs : finset.sum s (λ x, x.2) = n),
-  finset.prod s (λ x, ring_quot.mk_alg_hom R (rel R M) (mv_polynomial.X x)) = u}
+  { u : divided_power_algebra R M | ∃ (s : finset (ℕ × M)) (hs : finset.sum s (λ x, x.1) = n),
+  finset.prod s (λ x, ring_quot.mk_alg_hom R (rel R M) (X x)) = u}
 
 end divided_power_algebra
 
@@ -110,7 +108,7 @@ example : linear_equiv (ring_hom.id R) M (grade R M 1) := {
 /- Define the augmentation ideal : (proj' R M 0).ker -/
 
 /- and a divided power structure on that ideal such that
-dpow R n (ι R x) = ring_quot.mk_alg_hom R (rel R M) (mv_polynomial.X (x, n)) 
+dpow R n (ι R x) = ring_quot.mk_alg_hom R (rel R M) (X (x, n)) 
 
 (x,n) represents dpow n x
 dpow m (x,n) should be dpow m (dpow n x) = (mchoose m n) dpow (m*n) x
@@ -127,7 +125,7 @@ where the sum is over functions → ℕ, with sum k
 end divided_power_algebra
 
 /- Introduce notation ?
-Here : x ^ [n] = ring_quot.mk_alg_hom R _ (mv_polynomial.X (x, n))
+Here : x ^ [n] = ring_quot.mk_alg_hom R _ (X (x, n))
 In general, x ^ [n]  for dpow n x ? 
 
 -/
