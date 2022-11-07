d436f016f4338f41cca6c4108e9ee1e4a11d0d55
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2022-11-07 20:26:51 +0000

    prove comm_ring instance

--- src/divided_powers/dp_algebra.lean
+++ src/divided_powers/dp_algebra.lean
@@ -15,7 +15,7 @@ noncomputable theory
 /-! 
 The divided power algebra of a module -/
 
-open mv_polynomial
+open finset mv_polynomial ring_quot
 
 variables (R : Type*) [comm_ring R]
 variables (M : Type*) [add_comm_monoid M] [module R M]
@@ -28,7 +28,7 @@ inductive rel : mv_polynomial (ℕ × M) R → mv_polynomial (ℕ × M) R → Pr
 | smul {r : R} {n : ℕ} {a : M} : rel (X (n, r • a)) (r^n • X (n, a))
 | mul {m n : ℕ} {a : M} : rel (X (m, a) * X (n, a)) ((nat.choose (m + n) n) • X (m + n, a))
 | add {n : ℕ} {a b : M} : rel (X (n, a+b)) 
-  (finset.sum (finset.range (n + 1)) (λ k, (X (k, a) * X (n - k, b))))
+  (finset.sum (range (n + 1)) (λ k, (X (k, a) * X (n - k, b))))
 
 end divided_power_algebra
 
@@ -41,26 +41,38 @@ variable {M}
 
 /-- The canonical linear map `M →ₗ[R] divided_power_algebra R M`. -/
 def ι : M →ₗ[R] (divided_power_algebra R M) :=
-{ to_fun := λ m, (ring_quot.mk_alg_hom R _ (X (1, m))),
+{ to_fun := λ m, (mk_alg_hom R _ (X (1, m))),
   map_add' := λ x y, by { 
-    rw [←alg_hom.map_add, ring_quot.mk_alg_hom_rel R rel.add],
-    simp only [finset.sum_range_succ', finset.sum_range_zero, zero_add, nat.sub_zero,
+    rw [← map_add, mk_alg_hom_rel R rel.add],
+    simp only [sum_range_succ', sum_range_zero, zero_add, nat.sub_zero,
     nat.sub_self], 
-    simp only [alg_hom.map_add, alg_hom.map_mul],
-    simp only [ring_quot.mk_alg_hom_rel R rel.zero],
-    simp only [alg_hom.map_one, one_mul, mul_one], },
+    simp only [map_add, map_mul],
+    simp only [mk_alg_hom_rel R rel.zero],
+    simp only [map_one, one_mul, mul_one], },
   map_smul' := λ r x, by { 
-    rw [← alg_hom.map_smul, ring_quot.mk_alg_hom_rel R rel.smul, pow_one, alg_hom.map_smul],
-    simp only [ring_hom.id_apply, alg_hom.map_smul], } }
+    rw [← map_smul, mk_alg_hom_rel R rel.smul, pow_one, map_smul],
+    simp only [ring_hom.id_apply, map_smul], } }
 
-lemma ring_quot_mk_alg_hom_mv_polynomial_ι_eq_ι (m : M) :
-  ring_quot.mk_alg_hom R (rel R M) (X (1, m)) = ι R m := rfl
+lemma mk_alg_hom_mv_polynomial_ι_eq_ι (m : M) :
+  mk_alg_hom R (rel R M) (X (1, m)) = ι R m := rfl
 
 variable (M)
 def grade (n : ℕ) : submodule R (divided_power_algebra R M) :=
 submodule.span R 
   { u : divided_power_algebra R M | ∃ (s : finset (ℕ × M)) (hs : finset.sum s (λ x, x.1) = n),
-    finset.prod s (λ x, ring_quot.mk_alg_hom R (rel R M) (X x)) = u }
+    finset.prod s (λ x, mk_alg_hom R (rel R M) (X x)) = u }
+
+instance : has_one (grade R M 0) := 
+⟨⟨1, submodule.subset_span ⟨{(0, 0)}, by rw [sum_singleton], by
+  rw [prod_singleton, ← map_one (mk_alg_hom R (rel R M)), mk_alg_hom_rel R rel.zero]⟩⟩⟩
+
+instance : has_mul (grade R M 0) := 
+{ mul := λ x y, ⟨x*y, by sorry⟩ }
+
+@[simp] lemma grade_zero_coe_mul (x y : grade R M 0) :
+  ((x * y : grade R M 0) : divided_power_algebra R M) = (x : divided_power_algebra R M) * y := rfl
+
+@[simp] lemma grade_zero_coe_one: ((1 : grade R M 0) : divided_power_algebra R M) = 1 := rfl
 
 end divided_power_algebra
 
@@ -87,14 +99,15 @@ instance : comm_ring (grade R M 0) :=
   neg           := has_neg.neg,
   add_left_neg  := add_left_neg,
   add_comm      := add_comm,
-  one           := sorry,
-  mul           := sorry,
-  mul_assoc     := sorry,
-  one_mul       := sorry,
-  mul_one       := sorry,
-  left_distrib  := sorry,
-  right_distrib := sorry,
-  mul_comm      := sorry,
+  one           := 1,
+  mul           := (*),
+  mul_assoc     := λ x y z, by ext; simp only [grade_zero_coe_mul, mul_assoc],
+  one_mul       := λ x, by  ext; rw [grade_zero_coe_mul, grade_zero_coe_one, one_mul],
+  mul_one       := λ x, by  ext; rw [grade_zero_coe_mul, grade_zero_coe_one, mul_one],
+  left_distrib  := λ x y z, by ext; simp only [submodule.coe_add, grade_zero_coe_mul, left_distrib],
+  right_distrib := λ x y z, 
+    by ext; simp only [submodule.coe_add, grade_zero_coe_mul, right_distrib],
+  mul_comm      := λ x y, by ext; simp only [grade_zero_coe_mul, mul_comm],
   /- ..(grade R M 0).module -- Why isn't this working? -/ }
 
 
@@ -126,12 +139,12 @@ example : linear_equiv (ring_hom.id R) M (grade R M 1) :=
 /- Define the augmentation ideal : (proj' R M 0).ker -/
 
 /- and a divided power structure on that ideal such that
-dpow R n (ι R x) = ring_quot.mk_alg_hom R (rel R M) (X (x, n)) 
+dpow R n (ι R x) = mk_alg_hom R (rel R M) (X (x, n)) 
 
 (x,n) represents dpow n x
 dpow m (x,n) should be dpow m (dpow n x) = (mchoose m n) dpow (m*n) x
 An element x in divided_power_algebra R M takes the form
-ring_quot.mk_alg_hom R (rel R M) (P)
+mk_alg_hom R (rel R M) (P)
 where P is a sum of monomials  a * (m,n)   : m ∈ M, n ∈ ℕ
 define
 dpow k x = sum products a ^ kᵢ * dpow (mchoose kᵢ nᵢ (mᵢ,nᵢ * kᵢ)) 
@@ -143,7 +156,7 @@ where the sum is over functions → ℕ, with sum k
 end divided_power_algebra
 
 /- Introduce notation ?
-Here : x ^ [n] = ring_quot.mk_alg_hom R _ (X (x, n))
+Here : x ^ [n] = mk_alg_hom R _ (X (x, n))
 In general, x ^ [n]  for dpow n x ? 
 
 -/
