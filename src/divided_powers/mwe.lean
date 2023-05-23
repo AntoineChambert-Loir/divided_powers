b3d2dd692c3b64105a8730729800e1f3705a0cd5
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-05-22 21:27:25 -0600

    delete mwe file

--- src/divided_powers/mwe.lean
+++
@@ -1,6 +0,0 @@
-import algebra.module.basic
-
-variables (A : Type*) [comm_ring A] 
-
-def foo /- (A : Type*) [comm_ring A] -/ : Prop := 
-∃ (T : Type*) [comm_ring T], by exactI ∃ [module A T], true
