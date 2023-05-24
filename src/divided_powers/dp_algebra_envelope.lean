81c75675042b1d7e9892d59e01d55e572b44e764
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-05-23 20:19:18 -0600

    make universes explicit, fill in exists_dp_envelope

--- src/divided_powers/dp_algebra_envelope.lean
+++ src/divided_powers/dp_algebra_envelope.lean
@@ -1,27 +1,22 @@
 import divided_powers.dp_algebra
 import algebra_comp
 
+universes u v w
+
 open divided_powers ideal divided_power_algebra
 
-def is_universal {A : Type*} [comm_ring A] {I : ideal A} (hI : divided_powers I) {B : Type*}
-  [comm_ring B] [algebra A B] (J : ideal B) {D : Type*} [comm_ring D] (J' : ideal D)
-  (hJ' : divided_powers J') (ψ : B →+* D) (hψ : ψ '' J ⊆ J') :=
-∀ {C : Type*} [comm_ring C], by exactI ∀ (K : ideal C) (hK : divided_powers K)
-  (g : pd_morphism hI hK) (h : B →+* C) (hh : h '' J ⊆ K) 
+def is_universal {A : Type u} [comm_ring A] {I : ideal A} (hI : divided_powers I) {B : Type v}
+  [comm_ring B] [algebra A B] (J : ideal B) {D : Type v} [comm_ring D] (J' : ideal D)
+  (hJ' : divided_powers J') (ψ : B →+* D) (hψ : J.map ψ ≤ J') :=
+∀ (C : Type w) [comm_ring C], by exactI ∀ (K : ideal C) (hK : divided_powers K)
+  (g : pd_morphism hI hK) (h : B →+* C) (hh : J.map h ≤ K) 
   (hcomp : h ∘ (algebra_map A B) = g.to_ring_hom),
   ∃! (φ : pd_morphism hJ' hK), φ.to_ring_hom ∘ ψ = h 
 
--- Theorem 3.19 Berthelot-Ogus
-theorem exists_dp_envelope (A B : Type*) [comm_ring A] {I : ideal A} (hI : divided_powers I)
-  [comm_ring B] [algebra A B] (J : ideal B) :
-  ∃ (D : Type*) [hD : comm_ring D], by exactI ∃ {J' : ideal D} (hJ' : divided_powers J')
-  (ψ : B →+* D) (hψ : ψ '' J ⊆ J'), is_universal hI J J' hJ' ψ hψ :=
-sorry
-
 namespace divided_power_envelope
 
-variables {A B : Type*} [comm_ring A] {I : ideal A} (hI : divided_powers I) [comm_ring B] 
-  [algebra A B] (J : ideal B) 
+variables {A : Type u} [comm_ring A] {I : ideal A} (hI : divided_powers I) {B : Type v} 
+  [comm_ring B] [algebra A B] (J : ideal B) 
 
 section included
 
@@ -47,43 +42,47 @@ theorem J12_is_sub_pd_ideal : is_sub_pd_ideal (divided_power_algebra.divided_pow
   ((J12 hI J hIJ) ⊓ (aug_ideal B J)) :=
 sorry
 
-def dp_envelope := (divided_power_algebra B J) ⧸ (J12 hI J hIJ)
+def dp_envelope_of_included : Type v := (divided_power_algebra B J) ⧸ (J12 hI J hIJ)
 
-noncomputable instance : comm_ring (dp_envelope hI J hIJ) := ideal.quotient.comm_ring _
+noncomputable instance : comm_ring (dp_envelope_of_included hI J hIJ) := ideal.quotient.comm_ring _
 
-noncomputable instance : algebra B (dp_envelope hI J hIJ) := ideal.quotient.algebra _
+noncomputable instance : algebra B (dp_envelope_of_included hI J hIJ) := ideal.quotient.algebra _
 
-noncomputable instance algebra' : algebra A (dp_envelope hI J hIJ) := 
-algebra.comp A B (dp_envelope hI J hIJ)
+noncomputable instance algebra_of_included : algebra A (dp_envelope_of_included hI J hIJ) := 
+algebra.comp A B (dp_envelope_of_included hI J hIJ)
 
-instance : is_scalar_tower A B (dp_envelope hI J hIJ) :=
-is_scalar_tower.comp A B (dp_envelope hI J hIJ)
+instance is_scalar_tower_of_included : is_scalar_tower A B (dp_envelope_of_included hI J hIJ) :=
+is_scalar_tower.comp A B (dp_envelope_of_included hI J hIJ)
 
-noncomputable def dp_ideal : ideal (dp_envelope hI J hIJ) :=
+noncomputable def dp_ideal_of_included : ideal (dp_envelope_of_included hI J hIJ) :=
 (map (ideal.quotient.mk (J12 hI J hIJ)) (aug_ideal B J))
 
-lemma sub_ideal_dp_ideal : 
-  (algebra_map B (dp_envelope hI J hIJ)) '' J ⊆ (dp_ideal hI J hIJ) :=
+lemma sub_ideal_dp_ideal_of_included : 
+  J.map (algebra_map B (dp_envelope_of_included hI J hIJ)) ≤ (dp_ideal_of_included hI J hIJ) :=
 sorry
 
-theorem dp_envelope_is_universal : is_universal hI J (dp_ideal hI J hIJ)
+theorem dp_envelope_of_included_is_universal : is_universal hI J (dp_ideal_of_included hI J hIJ)
   (quot.divided_powers (divided_power_algebra.divided_powers' B J) (J12_is_sub_pd_ideal hI J hIJ))
-  (algebra_map B (dp_envelope hI J hIJ)) (sub_ideal_dp_ideal hI J hIJ) :=
+  (algebra_map B (dp_envelope_of_included hI J hIJ)) (sub_ideal_dp_ideal_of_included hI J hIJ) :=
 sorry
 
 end included
 
-namespace general
+section general
 
 variables (I)
 
 def J' : ideal B := J + I.map (algebra_map A B)
 
-lemma sub_ideal_J' :  I.map (algebra_map A B) ≤ J' I J := sorry
-
+lemma sub_ideal_J' :  I.map (algebra_map A B) ≤ J' I J := 
+begin
+  rw J',
+  sorry
+end
 variables {I}
 
-def dp_envelope := (divided_power_algebra B (J' I J)) ⧸ (J12 hI (J' I J) (sub_ideal_J' I J))
+def dp_envelope : Type v := 
+(divided_power_algebra B (J' I J)) ⧸ (J12 hI (J' I J) (sub_ideal_J' I J))
 
 noncomputable instance : comm_ring (dp_envelope hI J) := ideal.quotient.comm_ring _
 
@@ -98,7 +97,7 @@ is_scalar_tower.comp A B (dp_envelope hI J)
 noncomputable def dp_ideal : ideal (dp_envelope hI J) :=
 (ideal.map (ideal.quotient.mk (J12 hI (J' I J) (sub_ideal_J' I J))) (aug_ideal B (J' I J)))
 
-lemma sub_ideal_dp_ideal : (algebra_map B (dp_envelope hI J)) '' J ⊆ (dp_ideal hI J) :=
+lemma sub_ideal_dp_ideal : J.map (algebra_map B (dp_envelope hI J)) ≤ (dp_ideal hI J) :=
 sorry
 
 theorem dp_envelope_is_universal : is_universal hI J (dp_ideal hI J) 
@@ -107,6 +106,16 @@ theorem dp_envelope_is_universal : is_universal hI J (dp_ideal hI J)
   (algebra_map B (dp_envelope hI J)) (sub_ideal_dp_ideal hI J) :=
 sorry
 
-end general
+end general 
+
+-- Theorem 3.19 Berthelot-Ogus
+theorem exists_dp_envelope (A : Type u) [comm_ring A] {I : ideal A} (hI : divided_powers I)
+  (B : Type v) [comm_ring B] [algebra A B] (J : ideal B) :
+  ∃ (D : Type v) [hD : comm_ring D], by exactI ∃ {J' : ideal D} (hJ' : divided_powers J')
+  (ψ : B →+* D) (hψ : J.map ψ ≤ J'), is_universal hI J J' hJ' ψ hψ :=
+⟨(dp_envelope hI J), infer_instance, dp_ideal hI J,
+  (quot.divided_powers (divided_power_algebra.divided_powers' B (J' I J))
+    (J12_is_sub_pd_ideal hI (J' I J) (sub_ideal_J' I J))),
+algebra_map B _, sub_ideal_dp_ideal hI J, dp_envelope_is_universal hI J⟩
 
 end divided_power_envelope
