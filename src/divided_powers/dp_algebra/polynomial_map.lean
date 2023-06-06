248aba92e6b8f47f1804d02f303d9cc881ff9f7f
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-06-06 20:59:11 +0200

    delete universes

--- src/divided_powers/dp_algebra/polynomial_map.lean
+++ src/divided_powers/dp_algebra/polynomial_map.lean
@@ -30,7 +30,7 @@ def rid' : R ⊗[A] A →ₐ[R] R :=
 { map_one'  := by simp only [alg_equiv.to_fun_eq_coe, map_one], 
   map_zero' := by simp only [alg_equiv.to_fun_eq_coe, map_zero],
   commutes' := λ r, by simp only [algebra_map_apply, alg_equiv.to_fun_eq_coe, rid_tmul, one_smul], 
-  ..algebra.tensor_product.rid A R, }
+  ..algebra.tensor_product.rid A R }
 
 @[simp] lemma rid'_tmul (a : A) (r : R) : (rid' A R) (r ⊗ₜ[A] a) = a • r := rfl 
 
@@ -252,24 +252,31 @@ end mv_polynomial_module
 
 open_locale tensor_product
 
-universes u v₁ v₂ v₃ v₄ w w'
-variables {A : Type u} {M : Type v₁} {N : Type v₂} [comm_semiring A] [add_comm_monoid M] 
+--universes u v₁ v₂ v₃ v₄ w w'
+/- variables {A : Type u} {M : Type v₁} {N : Type v₂} [comm_semiring A] [add_comm_monoid M] 
+  [module A M] [add_comm_monoid N] [module A N] -/
+variables {A M N : Type*} [comm_semiring A] [add_comm_monoid M] 
   [module A M] [add_comm_monoid N] [module A N]
 
 /-- A polynomial map M → N between A-modules is a functorial family
 of maps R ⊗[A] M → R ⊗[A] N, for all A-algebras R -/
-@[ext] structure polynomial_map (A : Type u) [comm_semiring A] (M : Type v₁) [add_comm_monoid M]
-  [module A M]  (N : Type v₂) [add_comm_monoid N] [module A N] :=
-(to_fun : Π (R : Type w) [comm_semiring R], Π [by exactI algebra A R], by exactI 
+@[ext] structure polynomial_map (A : Type*) [comm_semiring A] (M : Type*) [add_comm_monoid M]
+  [module A M]  (N : Type*) [add_comm_monoid N] [module A N] :=
+(to_fun : Π (R : Type*) [comm_semiring R], Π [by exactI algebra A R], by exactI 
   (R ⊗[A] M → R ⊗[A] N))
-(is_compat : ∀ {R  : Type w} [comm_semiring R] [algebra A R] 
-  {R' : Type w} [comm_semiring R'] [algebra A R'] (φ : R →ₐ[A] R'), 
+(is_compat : ∀ {R  : Type*} [comm_semiring R] [algebra A R] 
+  {R' : Type*} [comm_semiring R'] [algebra A R'] (φ : R →ₐ[A] R'), 
   (φ.to_linear_map.rtensor N) ∘ (to_fun R) = (to_fun R') ∘ (φ.to_linear_map.rtensor M))
 
 namespace polynomial_map 
 
-lemma is_compat_apply (f : polynomial_map A M N) (R : Type w) [comm_semiring R] [algebra A R] 
+/- lemma is_compat_apply (f : polynomial_map A M N) (R : Type w) [comm_semiring R] [algebra A R] 
   (R' : Type w) [comm_semiring R'] [algebra A R'] (φ : R →ₐ[A] R') (x : R ⊗[A] M) : 
+  (φ.to_linear_map.rtensor N) ((f.to_fun R) x) = ((f.to_fun R') (φ.to_linear_map.rtensor M x)) :=
+by simpa only using congr_fun (f.is_compat φ) x-/
+
+lemma is_compat_apply (f : polynomial_map A M N) (R : Type*) [comm_semiring R] [algebra A R] 
+  (R' : Type*) [comm_semiring R'] [algebra A R'] (φ : R →ₐ[A] R') (x : R ⊗[A] M) : 
   (φ.to_linear_map.rtensor N) ((f.to_fun R) x) = ((f.to_fun R') (φ.to_linear_map.rtensor M x)) :=
 by simpa only using congr_fun (f.is_compat φ) x
 
@@ -284,17 +291,17 @@ instance : has_zero (polynomial_map A M N) :=
 ⟨ { to_fun := λ R _ _, 0, 
     is_compat := λ R _ _ R' _ _ φ, rfl }⟩ 
 
-@[simp] lemma zero_def (R : Type w) [comm_semiring R] [algebra A R] : 
+@[simp] lemma zero_def (R : Type*) [comm_semiring R] [algebra A R] : 
   (0 : polynomial_map A M N).to_fun R = 0 := rfl
 
 instance : inhabited (polynomial_map A M N) := ⟨has_zero.zero⟩
 
 instance : has_add (polynomial_map A M N) := ⟨add⟩
 
-@[simp] lemma add_def (f g : polynomial_map A M N) (R : Type w) [comm_semiring R] [algebra A R] : 
+@[simp] lemma add_def (f g : polynomial_map A M N) (R : Type*) [comm_semiring R] [algebra A R] : 
   (f + g).to_fun R = f.to_fun R + g.to_fun R := rfl
 
-@[simp] lemma add_def_apply (f g : polynomial_map A M N) (R : Type w) [comm_semiring R] 
+@[simp] lemma add_def_apply (f g : polynomial_map A M N) (R : Type*) [comm_semiring R] 
   [algebra A R] (m : R ⊗[A] M) : (f + g).to_fun R m = f.to_fun R m + g.to_fun R m := rfl
 
 instance add_comm_monoid : add_comm_monoid (polynomial_map A M N) := 
@@ -322,7 +329,7 @@ def smul (a : A) (f : polynomial_map A M N) : (polynomial_map A M N) :=
 
 instance has_smul : has_smul A (polynomial_map A M N) := ⟨smul⟩
 
-lemma smul_def (f : polynomial_map A M N) (a : A) (R : Type w) [comm_semiring R] [algebra A R] :  
+lemma smul_def (f : polynomial_map A M N) (a : A) (R : Type*) [comm_semiring R] [algebra A R] :  
   (a • f).to_fun R = a • (f.to_fun R) := rfl 
 
 instance : mul_action A (polynomial_map A M N) := 
@@ -341,20 +348,20 @@ end module
 
 section comp
 
-variables {P : Type v₃} [add_comm_monoid P] [module A P]
+variables {P : Type*} [add_comm_monoid P] [module A P]
 
 def comp (g : polynomial_map A N P) (f : polynomial_map A M N) : polynomial_map A M P :=
 { to_fun := λ R _ _, by exactI (g.to_fun R).comp (f.to_fun R),
   is_compat := λ R R' _ _ _ _ φ, by ext m; simp only [is_compat_apply, function.comp_app] }
 
-lemma comp_to_fun (f : polynomial_map A M N) (g : polynomial_map A N P) (R : Type w) 
+lemma comp_to_fun (f : polynomial_map A M N) (g : polynomial_map A N P) (R : Type*) 
   [comm_semiring R] [algebra A R] : (g.comp f).to_fun R = (g.to_fun R).comp (f.to_fun R) := rfl
 
-lemma comp_apply (f : polynomial_map A M N) (g : polynomial_map A N P) (R : Type w) 
+lemma comp_apply (f : polynomial_map A M N) (g : polynomial_map A N P) (R : Type*) 
   [comm_semiring R] [algebra A R] (m : R ⊗[A] M) : 
   (g.comp f).to_fun R m = (g.to_fun R) (f.to_fun R m) := rfl
 
-variables {Q : Type v₄} [add_comm_monoid Q] [module A Q]
+variables {Q : Type*} [add_comm_monoid Q] [module A Q]
 
 lemma comp_assoc (f : polynomial_map A M N) (g : polynomial_map A N P) (h : polynomial_map A P Q) :
   h.comp (g.comp f) = (h.comp g).comp f :=
@@ -380,7 +387,7 @@ def of_linear_map (v : M →ₗ[A] N) : polynomial_map A M N :=
   is_compat := λ R _ _ _ _ _ φ, by ext m; simp only [base_change_eq_ltensor, 
     ← linear_map.comp_apply, comp_app, rtensor_comp_ltensor, ltensor_comp_rtensor] }
 
-lemma of_linear_map_to_fun (u : M →ₗ[A] N) (R : Type w) [comm_semiring R] [algebra A R] :
+lemma of_linear_map_to_fun (u : M →ₗ[A] N) (R : Type*) [comm_semiring R] [algebra A R] :
   (of_linear_map u).to_fun R =base_change R u := rfl
 
 def of_linear_map_hom :  (M →ₗ[A] N) →ₗ[A] (polynomial_map A M N):= 
@@ -399,7 +406,7 @@ end linear
 section locally_finite
 
 def locfinsupp {ι : Type*} (f : ι → polynomial_map A M N) : Prop :=
-  ∀ (R : Type w) [comm_semiring R], by exactI ∀ [algebra A R], by exactI ∀ (m : R ⊗[A] M), 
+  ∀ (R : Type*) [comm_semiring R], by exactI ∀ [algebra A R], by exactI ∀ (m : R ⊗[A] M), 
   (function.support (λ i, (f i).to_fun R m)).finite
 
 variables (A M N)
@@ -444,7 +451,7 @@ noncomputable def sum {ι : Type*} (f : ι → polynomial_map A M N) (hf : locfi
       rw [hi, map_zero] },
   end }
 
-lemma sum_eq {ι : Type*} (f : ι → polynomial_map A M N) (hf : locfinsupp f) (R : Type w) 
+lemma sum_eq {ι : Type*} (f : ι → polynomial_map A M N) (hf : locfinsupp f) (R : Type*) 
   [comm_semiring R] [algebra A R] (m : R ⊗[A] M) : 
   (locfinsupp.sum f hf).to_fun R m = (finsupp.of_support_finite _ (hf R m)).sum (λ i m, m) := rfl
 
@@ -492,47 +499,39 @@ noncomputable def linear_map.locfinsupp.sum {ι : Type*} [decidable_eq ι] :
 
 end locally_finite
 
-#exit
-
-
 section coefficients
 
 variables {ι : Type*} [fintype ι] [decidable_eq ι]
 
 /-- The coefficients of a `polynomial_map` -/
 noncomputable def coeff' {ι : Type*} [fintype ι] [decidable_eq ι] (m : ι → M) (k : ι →₀ ℕ) : 
-  polynomial_map A M N  →ₗ[A] N := { 
-to_fun := λ f, tensor_product.lid A N ((linear_map.mv_polynomial.coeff k).rtensor N
-  (f.to_fun (mv_polynomial ι A)
-    (finset.univ.sum (λ i, (mv_polynomial.X i) ⊗ₜ[A] m i)))), 
-map_add' := λ f g, by simp only [add_def, pi.add_apply, map_add],
-map_smul' := λ a f, by simp only [smul_def, pi.smul_apply, linear_map.map_smulₛₗ, 
-  ring_hom.id_apply, linear_equiv.map_smulₛₗ] }
-
+  polynomial_map A M N →ₗ[A] N := 
+{ to_fun    := λ f, tensor_product.lid A N ((mv_polynomial.coeff_hom k).rtensor N
+    (f.to_fun (mv_polynomial ι A) (finset.univ.sum (λ i, (mv_polynomial.X i) ⊗ₜ[A] m i)))), 
+  map_add'  := λ f g, by simp only [add_def, pi.add_apply, map_add],
+  map_smul' := λ a f, by simp only [smul_def, pi.smul_apply, linear_map.map_smulₛₗ, 
+    ring_hom.id_apply, linear_equiv.map_smulₛₗ] }
 
 /-- The coefficients of a `polynomial_map` -/
-noncomputable def coeff {ι : Type*} [fintype ι] [decidable_eq ι] (m : ι → M) : polynomial_map A M N
-  →ₗ[A] ((ι →₀ ℕ) →₀ N) := {
-to_fun := λ f, zoo_inv _ A N (f.to_fun (mv_polynomial ι A)
-    (finset.univ.sum (λ i, (mv_polynomial.X i) ⊗ₜ[A] m i))),
-map_add' := λ f g, 
-by { rw ← map_add, refl, },
-map_smul' := λ a f, 
-by { simp only [ring_hom.id_apply, ← map_smul], refl, }, }
+noncomputable def coeff {ι : Type*} [fintype ι] [decidable_eq ι] (m : ι → M) : 
+  polynomial_map A M N →ₗ[A] ((ι →₀ ℕ) →₀ N) := 
+{ to_fun    := λ f, zoo_inv _ A N (f.to_fun (mv_polynomial ι A)
+      (finset.univ.sum (λ i, (mv_polynomial.X i) ⊗ₜ[A] m i))),
+  map_add'  := λ f g, by { rw ← map_add, refl, },
+  map_smul' := λ a f, by { simp only [ring_hom.id_apply, ← map_smul], refl, }, }
 
-lemma coeff_eq (m : ι → M) (k : ι →₀ ℕ) 
-  (f : polynomial_map A M N) :
-  coeff m f k = 
-  (tensor_product.lid A N)
-  ((linear_map.rtensor N (linear_map.mv_polynomial.coeff k))
-     (f.to_fun (mv_polynomial ι A) (finset.univ.sum (λ (i : ι), mv_polynomial.X i ⊗ₜ[A] m i)))) :=
+
+
+lemma coeff_eq (m : ι → M) (k : ι →₀ ℕ) (f : polynomial_map A M N) :
+  coeff m f k = (tensor_product.lid A N) ((linear_map.rtensor N (mv_polynomial.coeff_hom k))
+      (f.to_fun (mv_polynomial ι A) (finset.univ.sum (λ (i : ι), mv_polynomial.X i ⊗ₜ[A] m i)))) :=
 by simp only [coeff, linear_map.coe_mk, zoo_inv, zoo_inv', finsupp.of_support_finite_coe]
 
 
 theorem image_eq_coeff_sum 
   {ι : Type*} [fintype ι] [decidable_eq ι] (m : ι → M) 
   (f : polynomial_map A M N) 
-  (R : Type w) [comm_semiring R] [algebra A R] (r : ι → R) :
+  (R : Type*) [comm_semiring R] [algebra A R] (r : ι → R) :
   f.to_fun R (finset.univ.sum (λ i, r i ⊗ₜ[A] m i)) =
 (coeff m f).sum (λ k n, finset.univ.prod (λ i, r i ^ (k i)) ⊗ₜ[A] n) := 
 begin
@@ -545,7 +544,7 @@ begin
     alg_hom.to_linear_map_apply, mv_polynomial.aeval_X] at that, 
   rw ← that, rw this, 
   simp only [finsupp.sum], 
-  rw map_sum,  
+  rw _root_.map_sum,  
   apply finset.sum_congr rfl,
   intros k hk, simp only [linear_map.rtensor_tmul, alg_hom.to_linear_map_apply], 
   apply congr_arg2 _ _ rfl,
@@ -559,12 +558,14 @@ begin
   rw zoo_inv_zoo_apply, refl,
 end
 
+variables {R : Type*} [comm_semiring R] [algebra A R]
+
 lemma span_tensor_product_eq_top_of_span_eq_top
   (σ : Type*) (e : σ → M) (hm : submodule.span A (set.range e) = ⊤) :
   (submodule.span R (set.range (λ s, (1 : R) ⊗ₜ[A] (e s))) : 
   submodule R (R ⊗[A] M)) = ⊤ :=
 begin
-  rw eq_top_iff,
+  rw _root_.eq_top_iff,
   intros m h,
   induction m using tensor_product.induction_on with r m x y hx hy,
   exact zero_mem _,
@@ -603,7 +604,7 @@ begin
   simp [tensor_product.smul_tmul'] at h,
   obtain ⟨r, rfl⟩ := h, 
   rw finsupp.sum_of_support_subset _ (finset.subset_univ _), 
-  rw image_eq_coeff_sum r m f, 
+  rw image_eq_coeff_sum m f, 
   simp only [image_eq_coeff_sum], rw h,
   intros i hi, simp only [tensor_product.zero_tmul],
 end
@@ -619,7 +620,7 @@ begin
   ext m,
   dsimp,
   simp only [finsupp.sum],
-  rw map_sum,
+  rw _root_.map_sum,
   apply finset.sum_congr rfl,
 
   intros k hk,
@@ -676,7 +677,7 @@ lemma finsup_polynomial_map_of_coeff (b : basis ι A M) (f : polynomial_map A M 
   finsupp.polynomial_map b (coeff (coe_fn b) f) = f :=
 begin
   apply coeff_injective  (coe_fn b),
-  { rw eq_top_iff, intros m hm,
+  { rw _root_.eq_top_iff, intros m hm,
     apply submodule.span_mono _ (basis.mem_span_repr_support b m),
     apply set.image_subset_range, },
   rw coeff_of_finsup_polynomial_map, 
