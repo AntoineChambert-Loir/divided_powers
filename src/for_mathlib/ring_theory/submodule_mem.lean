2f4c973535aaf2e9200d463a9a87fcde05050a7a
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-07-13 11:50:47 +0200

    add namespace and lint

--- src/for_mathlib/ring_theory/submodule_mem.lean
+++ src/for_mathlib/ring_theory/submodule_mem.lean
@@ -1,40 +1,30 @@
-import ring_theory.ideal.operations 
+import ring_theory.ideal.operations
 
+namespace submodule
 
-
-theorem submodule.mem_span_iff_exists_sum {R : Type*} {M : Type*} 
-  [comm_semiring R] [add_comm_monoid M] [module R M] 
-  {ι : Type*} (f : ι → M) (x : M) :
-x ∈ submodule.span R (set.range f) ↔ 
-∃ (a : ι →₀ R), a.sum (λ (i : ι) (c : R), c • f i) = x :=
+theorem mem_span_iff_exists_sum {R : Type*} [comm_semiring R] {M : Type*} [add_comm_monoid M] 
+  [module R M] {ι : Type*} (f : ι → M) (x : M) :
+  x ∈ span R (set.range f) ↔ ∃ (a : ι →₀ R), a.sum (λ (i : ι) (c : R), c • f i) = x :=
 begin
-  rw ← submodule.top_smul (submodule.span R (set.range f)),  
-  rw submodule.mem_ideal_smul_span_iff_exists_sum,
-  apply exists_congr,
-  exact λ a, ⟨λ ⟨ha, h⟩, h, λ h, ⟨λ i, submodule.mem_top, h⟩⟩,
+  rw [← top_smul (span R (set.range f)), mem_ideal_smul_span_iff_exists_sum],
+  exact exists_congr (λ a, ⟨λ ⟨ha, h⟩, h, λ h, ⟨λ i, mem_top, h⟩⟩),
 end
 
-theorem submodule.mem_span_iff_exists_sum' {R : Type*} {M : Type*} 
-  [comm_semiring R] [add_comm_monoid M] [module R M] 
-  {ι : Type*} (s : set ι) (f : ι → M) (x : M) :
-x ∈ submodule.span R (f '' s) ↔ 
-∃ (a : ↥s →₀ R), a.sum (λ (i : ↥s) (c : R), c • f ↑i) = x :=
+theorem mem_span_iff_exists_sum' {R : Type*} [comm_semiring R] {M : Type*} [add_comm_monoid M]
+  [module R M] {ι : Type*} (s : set ι) (f : ι → M) (x : M) :
+  x ∈ span R (f '' s) ↔ ∃ (a : ↥s →₀ R), a.sum (λ (i : ↥s) (c : R), c • f ↑i) = x :=
 begin
-  rw ← submodule.top_smul (submodule.span R (f '' s)),  
-  rw submodule.mem_ideal_smul_span_iff_exists_sum',
-  apply exists_congr,
-  exact λ a, ⟨λ ⟨ha, h⟩, h, λ h, ⟨λ i, submodule.mem_top, h⟩⟩,
+  rw [← top_smul (span R (f '' s)), mem_ideal_smul_span_iff_exists_sum'],
+  exact exists_congr (λ a, ⟨λ ⟨ha, h⟩, h, λ h, ⟨λ i, mem_top, h⟩⟩)
 end
 
-theorem submodule.mem_span_set_iff_exists_sum {R : Type*} {M : Type*} 
-  [comm_semiring R] [add_comm_monoid M] [module R M] 
-  (s : set M) (x : M) :
-x ∈ submodule.span R s ↔ 
-∃ (a : s →₀ R), a.sum (λ (i : s) (c : R), c • ↑i) = x :=
+theorem mem_span_set_iff_exists_sum {R : Type*} [comm_semiring R] {M : Type*} [add_comm_monoid M]
+  [module R M] (s : set M) (x : M) :
+  x ∈ span R s ↔ ∃ (a : s →₀ R), a.sum (λ (i : s) (c : R), c • ↑i) = x :=
 begin
   conv_lhs {rw ← set.image_id s},
-  rw ← submodule.top_smul (submodule.span R (id '' s)),  
-  rw submodule.mem_ideal_smul_span_iff_exists_sum',
-  apply exists_congr,
-  exact λ a, ⟨λ ⟨ha, h⟩, h, λ h, ⟨λ i, submodule.mem_top, h⟩⟩,
+  rw [← top_smul (span R (id '' s)), mem_ideal_smul_span_iff_exists_sum'],
+  exact exists_congr (λ a, ⟨λ ⟨ha, h⟩, h, λ h, ⟨λ i, mem_top, h⟩⟩),
 end
+
+end submodule
